82012527006e15d3e7d6985effdf542239ee2f05
wanghuan <196912334@qq.com>
2019-08-30 20:59:19 +0800

    any 类型修改

--- lib/form/validator.tsx
+++ lib/form/validator.tsx
@@ -1,76 +1,96 @@
-import { FormValue } from './form';
+import { FormValue } from "./form";
 
 type FormRules = Array<FormRule>;
 interface FormRule {
-  key: string,
-  required?: boolean,
-  minLength?: number,
-  maxLength?: number,
-  pattern?: RegExp,
-  validator?: (value:string) => Promise<string>,
+  key: string;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  pattern?: RegExp;
+  validator?: (value: string) => Promise<string>;
 }
 
-type OneError = string | Promise<string>; 
+type OneError = string | Promise<string>;
 
-const validator = (Formvalue: FormValue, rules: FormRules, callback: (error:any) => void): void => {
-  const errors: any = {}
+const validator = (
+  Formvalue: FormValue,
+  rules: FormRules,
+  callback: (error: { [key: string]: string[] }) => void
+): void => {
+  const errors: { [key: string]: OneError[] } = {};
 
   const isEmpty = (value: any) => {
-    return value === undefined || value === null || value === '';
-  }
+    return value === undefined || value === null || value === "";
+  };
   const setError = (key: string, error: OneError) => {
     if (isEmpty(errors[key])) {
-      errors[key] = []
+      errors[key] = [];
     }
-    errors[key].push(error)
-  }
+    errors[key].push(error);
+  };
   rules.map(rule => {
     const value = Formvalue[rule.key];
-    if(rule.validator){
-      const promise = rule.validator(value)
-      setError(rule.key, promise)
+    if (rule.validator) {
+      const promise = rule.validator(value);
+      setError(rule.key, promise);
     }
     if (rule.required && isEmpty(value)) {
-      setError(rule.key, 'required')
+      setError(rule.key, "required");
     }
     if (rule.minLength && !isEmpty(value) && value.length < rule.minLength) {
-      setError(rule.key, 'minLength')
+      setError(rule.key, "minLength");
     }
     if (rule.maxLength && !isEmpty(value) && value.length > rule.maxLength) {
-      setError(rule.key, 'maxLength')
+      setError(rule.key, "maxLength");
     }
     if (rule.pattern && !isEmpty(value) && !rule.pattern.test(value)) {
-      setError(rule.key, 'pattern')
+      setError(rule.key, "pattern");
     }
-  })
+  });
 
-  const flattenErrors = flat(Object.keys(errors).map(key => errors[key].map((promise: any) => [key, promise])))
-  const newPromise = flattenErrors.map(([key, promiseOrString]) => (promiseOrString instanceof Promise ? promiseOrString : Promise.reject(promiseOrString))
-  .then(() => [key, undefined], (reason) => [key, reason]))
-
+  const flattenErrors = flat(
+    Object.keys(errors).map(key =>
+      errors[key].map<[string, OneError]>(error => [key, error])
+    )
+  );
+  const newPromise = flattenErrors.map(([key, promiseOrString]) =>
+    (promiseOrString instanceof Promise
+      ? promiseOrString
+      : Promise.reject(promiseOrString)
+    ).then<[string, undefined], [string, string]>(
+      () => [key, undefined],
+      reason => [key, reason]
+    )
+  );
+  // 类型守卫
+  function hasError(
+    item: [string, undefined] | [string, string]
+  ): item is [string, string] {
+    return typeof item[1] === "string";
+  }
   Promise.all(newPromise).then(result => {
-    callback(zip(result.filter((item: any[]) => item[1])))
-  })
-}
+    callback(zip(result.filter<[string, string]>(hasError)));
+  });
+};
 
-export default validator
+export default validator;
 
-function flat(array: Array<any>){
-  let result = []
-  for(let i=0; i<array.length; i++){
-    if(array[i] instanceof Array){
-      result.push(...array[i])
-    }else{
-      result.push(array[i])
+function flat<T>(array: Array<T | T[]>) {
+  let result: T[] = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] instanceof Array) {
+      result.push(...(array[i] as T[]));
+    } else {
+      result.push(array[i] as T);
     }
   }
-  return result
+  return result;
 }
-function zip(array: any[][]){
-  let result:any = {}
+function zip(array: Array<[string, string]>) {
+  let result: { [key: string]: string[] } = {};
   array.map(([key, value]) => {
-    result[key] = result[key] || []
-    result[key].push(value)
-  })
-  return result
+    result[key] = result[key] || [];
+    result[key].push(value);
+  });
+  return result;
 }
